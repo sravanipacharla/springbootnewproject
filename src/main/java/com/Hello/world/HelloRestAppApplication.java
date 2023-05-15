c5c1e5eda6ca4818f700d6746bea4c5dec68055f
sravanipacharla <sravanipacharla83@gmail.com>
2023-05-14 23:09:45 -0800

    first commit

---
+++ src/main/java/com/Hello/world/HelloRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.Hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestAppApplication.class, args);
+	}
+
+}
